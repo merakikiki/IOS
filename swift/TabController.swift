4224a80027991374f70a0364b0894ff64f410705
meraki <119564509+merakikiki@users.noreply.github.com>
2023-12-20 23:51:59 +0600

    Add files via upload

---
+++ swift/TabController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+
+class TabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+            let firstViewController = ViewController()
+            let secondViewController = ViewController2()
+
+        viewControllers = [firstViewController, secondViewController]
+
+        setTabBarItem(for: firstViewController, title: "Home", symbolName: "house")
+                setTabBarItem(for: secondViewController, title: "Favourite", symbolName: "heart")
+            }
+    private func setTabBarItem(for viewController: UIViewController, title: String, symbolName: String) {
+            let symbolConfiguration = UIImage.SymbolConfiguration(pointSize: 22, weight: .regular)
+            let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
+            viewController.tabBarItem = UITabBarItem(title: title, image: image, selectedImage: image)
+        }
+    }

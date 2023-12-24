05e1d4711e611c51127555d77bbda2321c7a5cfb
meraki <119564509+merakikiki@users.noreply.github.com>
2023-12-24 23:45:42 +0600

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,231 +1,192 @@
 import UIKit
 import SnapKit
 
-final class ViewController: UIViewController {
+class ViewController: UIViewController {
     
-    private let names = ["IOS Task", "Data Analysis Assignment", "UI Homework", "Mat.Stat Practice", "Mat.Stat Assignment", "Midterm Data Analysis", "Yoga", "Stretching", "Cleaning day", "UI Presentation", "Big Theme Discussion"]
-    private let images = ["Any 2", "Any 4", "Any 3", "Any 5", "Any 6", "Any 7", "Any 8", "Any 9", "Any 10", "Any 11", "Any 12"]
-    
-    private let sections = ["To Do", "Done(10)", "In Progress(8)"]
-
-    
-    private lazy var namesTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.register(NameTableViewCell.self, forCellReuseIdentifier: "NameTableViewCell")
-        return tableView
-    }()
-    
-    private lazy var doneTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.register(NameTableViewCell.self, forCellReuseIdentifier: "NameTableViewCell")
-        return tableView
-    }()
-    
-    private lazy var inProgressTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.register(NameTableViewCell.self, forCellReuseIdentifier: "NameTableViewCell")
-        return tableView
-    }()
-    
-    private lazy var ToDo: UILabel = {
-        let label = UILabel()
-        label.text = "To Do List"
-        label.textColor = .black
-        label.font = .boldSystemFont(ofSize: 30)
-        label.textAlignment = .center
-        return label
-    }()
-
-    private lazy var tasks: UILabel = {
-        let label = UILabel()
-        label.text = "8 tasks, 3 completed"
-        label.textColor = .lightGray
-        return label
-    }()
-    
-    private lazy var calendar: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "Any")
-        return imageView
-    }()
-    
-    private var search: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "Any 1")
-        return imageView
-    }()
-    
-    private lazy var yest: UILabel = {
-        let label = UILabel()
-        label.text = "YESTERDAY"
-        label.textColor = .black
-        
-        return label
-    }()
-    
-    private lazy var today: UILabel = {
-        let label = UILabel()
-        label.text = "TODAY"
-        label.textColor = .white
-        label.backgroundColor = .black
-        label.layer.cornerRadius = 15
-        label.layer.masksToBounds = true
-        label.textAlignment = .center
-        return label
-    }()
-
-
-    
-    private lazy var tomorrow: UILabel = {
-        let label = UILabel()
-        label.text = "TOMORROW"
-        label.textColor = .black
-        return label
-    }()
-    
-    private lazy var kiki: UIView = {
-        let view = UIView()
-        view.backgroundColor = .lightGray
-        return view
-    }()
-    
-    
+    private let backgroundColor = UIColor(named: "BackgroundColor")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
-        namesTableView.dataSource = self
-        namesTableView.delegate = self
+        view.backgroundColor = .white
         
         setupUI()
     }
-}
-
-extension ViewController: UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 10
-        case 1:
-            return 5
-        case 2:
-            return 5
-        default:
-            return 0
-        }
-    }
+    private lazy var iconView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "icon")
+        return imageView
+    }()
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
-    }
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Joined"
+        label.textColor = .lightGray
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
+    }()
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section]
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "NameTableViewCell", for: indexPath) as! NameTableViewCell
-
-        switch indexPath.section {
-        case 0:
-            cell.configure(name: names[indexPath.row], imageName: images[indexPath.row])
-        case 1:
-            cell.configure(name: names[indexPath.row], imageName: images[indexPath.row])
-        default:
-            cell.configure(name: names[indexPath.row], imageName: images[indexPath.row])
-        }
-        return cell
-    }
-}
-
-
-//MARK: - Table view delegate methods
-extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        (view.window?.windowScene?.screen.bounds.height ?? 0) * 0.1
-    }
-}
-
-// MARK: - UI setup methods
-private extension ViewController {
+    private lazy var titleLabell: UILabel = {
+        let label = UILabel()
+        label.text = "2 years ago"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 30)
+        return label
+    }()
     
-    func setupUI() {
-        setupViews()
+    private lazy var titleLabel2: UILabel = {
+        let label = UILabel()
+        label.text = "Belgibayeva"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 40)
+        return label
+    }()
+    
+    private lazy var titleLabel5: UILabel = {
+        let label = UILabel()
+        label.text = "Qaraqat"
+        label.textColor = .lightGray
+        label.font = .boldSystemFont(ofSize: 40)
+        return label
+    }()
+    
+    private lazy var titleLabel3: UILabel = {
+        let label = UILabel()
+        label.text = "Profile"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 30)
+        return label
+    }()
+    
+    private lazy var manageUserButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Manage user", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
+    
+    private lazy var titleLabel6: UILabel = {
+        let label = UILabel()
+        label.text = "Settings"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 30)
+        return label
+    }()
+    
+    private lazy var manageUserButton2: UIButton = {
+        let button = UIButton()
+        button.setTitle("Notification", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
+    private lazy var manageUserButton3: UIButton = {
+        let button = UIButton()
+        button.setTitle("Dark Mode", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
+    
+    private func setupUI() {
+        view.addSubview(iconView)
+        view.addSubview(titleLabel)
+        view.addSubview(titleLabell)
+        view.addSubview(titleLabel2)
+        view.addSubview(titleLabel3)
+        view.addSubview(titleLabel5)
+        view.addSubview(titleLabel6)
+        view.addSubview(manageUserButton)
+        view.addSubview(manageUserButton2)
+        view.addSubview(manageUserButton3)
+        
+        
         setupConstraints()
     }
     
-    func setupViews() {
-        view.addSubview(namesTableView)
-        view.addSubview(ToDo)
-        view.addSubview(tasks)
-        view.addSubview(calendar)
-        view.addSubview(search)
-        view.addSubview(kiki)
-        kiki.addSubview(yest)
-        kiki.addSubview(today)
-        kiki.addSubview(tomorrow)
-        
-    }
-    
-
-
-    
-    func setupConstraints() {
-        namesTableView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(10)
-            make.top.equalTo(yest.snp.bottom).offset(20)
-            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+    private func setupConstraints() {
+        iconView.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(30)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
+            $0.width.equalTo(120)
+            $0.height.equalTo(120)
         }
         
-        ToDo.snp.makeConstraints{ make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            make.leading.equalTo(calendar.snp.trailing).offset(50)
+        iconView.layer.cornerRadius = 60
+        iconView.layer.masksToBounds = true
+        
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(iconView)
+            $0.left.equalTo(iconView.snp.right).offset(30)
+            // Adjust the offset as needed
         }
         
-        tasks.snp.makeConstraints { make in
-            make.top.equalTo(calendar.snp.bottom).inset(20)
-            make.leading.equalTo(115)
+        titleLabell.snp.makeConstraints {
+            
+            $0.left.equalTo(iconView.snp.right).offset(30)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(5)
+        }
+        //2 years ago
+        
+        titleLabel2.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(iconView.snp.top).offset(200)
+            $0.left.equalTo(iconView)
         }
         
-        calendar.snp.makeConstraints{ make in
-            make.leading.equalTo(20)
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        titleLabel5.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel2.snp.bottom).offset(-10)
+            $0.left.equalTo(iconView)
         }
         
-        search.snp.makeConstraints{ make in
-            make.leading.equalTo(ToDo.snp.trailing).offset(70)
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(5)
+        titleLabel3.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(manageUserButton.snp.top).offset(-10)
+            $0.left.equalTo(30) // profile
+            
         }
         
-        yest.snp.makeConstraints{ make in
-            make.leading.equalTo(20)
-            make.centerY.equalTo(kiki.snp.centerY)
-           
+        manageUserButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.left.equalTo(30) //manage user
         }
         
-        today.snp.makeConstraints { make in
-            make.leading.equalTo(yest.snp.trailing).offset(50)
-            make.centerY.equalTo(kiki.snp.centerY)
-            make.trailing.equalTo(tomorrow.snp.leading).offset(-50)
-            make.width.equalTo(80)
-            make.height.equalTo(30)
-        }
-
-        
-        tomorrow.snp.makeConstraints{ make in
-            make.trailing.equalTo(-20)
-            make.centerY.equalTo(kiki.snp.centerY)
+        titleLabel6.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(manageUserButton.snp.bottom).offset(20)
+            $0.left.equalTo(30) //settings
             
         }
         
         
-        kiki.snp.makeConstraints { make in
-            make.width.equalToSuperview().multipliedBy(1)
-            make.height.equalToSuperview().multipliedBy(0.05)
-            make.top.equalTo(tasks.snp.bottom).offset(20)                      }
+        manageUserButton2.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel6.snp.bottom).offset(20) //notifications
+            $0.left.equalTo(titleLabel6)
+        }
         
+        manageUserButton3.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(manageUserButton2.snp.bottom).offset(20)
+            $0.left.equalTo(titleLabel6) //dark mode
+            
+            
+        }
     }
     
 }
